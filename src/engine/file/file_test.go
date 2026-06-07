f3980e1ce2d4e8984ef44d74a9fc42b2bbd52a52
Kisura <85266425+KisuraWSP@users.noreply.github.com>
2026-06-07 17:53:42 +0530

    added a basic test to check the program

--- src/engine/file/file_test.go
+++ src/engine/file/file_test.go
@@ -1,1 +1,21 @@
 package file
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestCheckForExistingFile(t *testing.T) {
+	if !FileExists("tests/test1.klang") {
+		t.Error("This file exists")
+	}
+}
+
+func TestCheckForNonExistingFile(t *testing.T) {
+	result := FileExists("tests/var.klang")
+	fmt.Printf("\n--> Debug: Checking 'tests/var.klang'. Result was: %v\n", result)
+
+	if result {
+		t.Error("This file Doesn't Exist, but FileExists returned true")
+	}
+}
